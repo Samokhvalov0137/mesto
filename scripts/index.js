f8fa64173d50fc41933aac0c014c804fa4e726f9
Vlad Samokhvalov <samokhvalov.v.i@yandex.ru>
2022-05-13 12:32:32 +0800

    Правки "можно лучше" из 1 ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,4 @@
-// переменные
-
+// ПЕРЕМЕННЫЕ
 const buttonOpenPopupEdit = document.querySelector(".profile__button-edit");
 const popupEdit = document.querySelector(".popup");
 const closePopupEditButton = document.querySelector(".popup__close");
@@ -12,12 +11,10 @@ const jobInput = document.querySelector('input[name="form_status"]');
 const popupCard = document.querySelector("#popup-add");
 const closePopupAddCard = document.querySelector("#popup-add__close");
 const openPopupAddCard = document.querySelector(".profile__button-add");
-
 // шаблоны
 const initialCardTemplate = document
   .querySelector("#elements__template")
   .content.querySelector(".element");
-
 // ДОМ элементы
 const initialContainer = document.querySelector(".elements");
 const popupFormAdd = document.querySelector("#form__add");
@@ -25,32 +22,27 @@ const placeInput = document.querySelector("#input_add_place");
 const linkInput = document.querySelector("#input_add_link");
 const photoPopupCard = document.querySelector(".popup__image");
 const namePopupCard = document.querySelector(".popup__name");
-
-// ПОПАП ПРОСМОТРА КАРТИНКИ
+// попап просмотра картинки
 const popupCardPhoto = document.querySelector(".popup_images");
 const closePopupCardPhotoButton = document.querySelector("#popup-card__close");
 
-// ПОПАП РЕДАКТИРОВАНИЯ ИМЕНИ
+// ФУНКЦИИ
 // функция открытия попапа редактирования имени
 const openPopup = (popupName) => {
   popupName.classList.add("popup_opened");
 };
 
+// функция для подставления значений в попап редактирования
 function handleOpenPopupEdit() {
   nameInput.value = profileName.textContent;
   jobInput.value = profileStatus.textContent;
   openPopup(popupEdit);
 }
 
-buttonOpenPopupEdit.addEventListener("click", handleOpenPopupEdit);
-buttonOpenPopupEdit.addEventListener("click", () => openPopup(popupEdit));
-
 // функция закрытия попапа редактирования имени
 const closePopup = (popupName) => {
   popupName.classList.remove("popup_opened");
 };
-
-closePopupEditButton.addEventListener("click", () => closePopup(popupEdit));
 
 // функция отправки и кнопки "сохранить" в попапе редактирования имени
 function handleProfileFormSubmit(evt) {
@@ -61,9 +53,6 @@ function handleProfileFormSubmit(evt) {
   closePopup(popupEdit);
 }
 
-formProfileEdit.addEventListener("submit", handleProfileFormSubmit);
-
-// ПОПАП ДОБАВЛЕНИЯ КАРТОЧЕК
 // функция открытия попапа добавления карточек
 openPopupAddCard.addEventListener("click", () => openPopup(popupCard));
 
@@ -75,41 +64,21 @@ closePopupCardPhotoButton.addEventListener("click", () =>
   closePopup(popupCardPhoto)
 );
 
+// функция для лайка
+const handleLikeCard = (event) => {
+  event.target.classList.toggle("element__vector_active");
+};
 
-// массив карточек с фото и названиями
-const initialCards = [
-  {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-  },
-  {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-  },
-  {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-  },
-  {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-  },
-  {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-  },
-  {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-  },
-];
+// функция для корзины
+const handleDeleteCard = (event) => {
+  event.target.closest(".element").remove();
+};
 
-// обработчики событий
-
-const handleSubmitAddInitialForm = (event) => {
+// функция отправки формы для создания карточек
+const handleAddCardFormSubmit = (event) => {
   event.preventDefault();
 
-  renderInitialCard({
+  renderCard({
     name: placeInput.value,
     link: linkInput.value,
   });
@@ -120,17 +89,20 @@ const handleSubmitAddInitialForm = (event) => {
   closePopup(popupCard);
 };
 
-const handleLikeCard = (event) => {
-  event.target.classList.toggle("element__vector_active");
-};
 
-const handleDeleteCard = (event) => {
-  event.target.closest(".element").remove();
-};
+// ОБРАБОТЧИКИ СОБЫТИЙ
+
+// кнопки открыть и закрыть для попапа редактирования
+buttonOpenPopupEdit.addEventListener("click", handleOpenPopupEdit);
+buttonOpenPopupEdit.addEventListener("click", () => openPopup(popupEdit));
+closePopupEditButton.addEventListener("click", () => closePopup(popupEdit));
+
+// слушатель кнопки "сохранить" в попапе редактирования имени
+formProfileEdit.addEventListener("submit", handleProfileFormSubmit);
 
 //генерация карточки
 
-const generateInitialCard = (initialData) => {
+const generateCard = (initialData) => {
   const newInitialCard = initialCardTemplate.cloneNode(true);
 
   const elementTitle = newInitialCard.querySelector(".element__title");
@@ -140,13 +112,20 @@ const generateInitialCard = (initialData) => {
   elementPhoto.src = initialData.link;
   elementPhoto.alt = initialData.name;
 
-  elementPhoto.addEventListener("click", function () {
+
+  // функция нажатия на картинку в карточке
+  function hendleTapCard (){
     photoPopupCard.src = initialData.link;
     namePopupCard.textContent = initialData.name;
     photoPopupCard.alt = initialData.name;
-
+  
     openPopup(popupCardPhoto);
-  });
+  };
+
+  
+
+
+  elementPhoto.addEventListener("click",hendleTapCard);
 
   // like card
   const likeButton = newInitialCard.querySelector(".element__vector");
@@ -160,13 +139,12 @@ const generateInitialCard = (initialData) => {
 };
 
 //Рендер карточки
-const renderInitialCard = (initialData) => {
-  initialContainer.prepend(generateInitialCard(initialData));
+const renderCard = (initialData) => {
+  initialContainer.prepend(generateCard(initialData));
 };
 
 initialCards.forEach((initialData) => {
-  renderInitialCard(initialData);
+  renderCard(initialData);
 });
 
-popupFormAdd.addEventListener("submit", handleSubmitAddInitialForm);
-
+popupFormAdd.addEventListener("submit", handleAddCardFormSubmit);
