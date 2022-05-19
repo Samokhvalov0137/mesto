e6edaf5e1dc2b63a36a9214f388b9814d05fde85
Vlad Samokhvalov <samokhvalov.v.i@yandex.ru>
2022-05-19 12:25:06 +0800

    Место. ПР 6.

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,11 @@
 // ПЕРЕМЕННЫЕ
+const popup = document.querySelector('.popup');
 const buttonOpenPopupEdit = document.querySelector(".profile__button-edit");
-const popupEdit = document.querySelector(".popup");
+const popupEdit = document.querySelector("#popup-edit");
 const closePopupEditButton = document.querySelector(".popup__close");
 const profileName = document.querySelector(".profile__name");
 const profileStatus = document.querySelector(".profile__status");
-const formProfileEdit = document.querySelector(".form");
+const formProfileEdit = document.querySelector("#form-edit");
 const nameInput = document.querySelector('input[name="form_name"]');
 const jobInput = document.querySelector('input[name="form_status"]');
 // переменные попапа добавления карточки
@@ -26,8 +27,14 @@ const namePopupCard = document.querySelector(".popup__name");
 const popupCardPhoto = document.querySelector(".popup_images");
 const closePopupCardPhotoButton = document.querySelector("#popup-card__close");
 
+//валидация
+//const form = document.querySelector('.form');
+//const submit = document.querySelector('.form__submit-btn');
+//const name = document.querySelector('#form_name');
+//const status = document.querySelector('#form_status');
+
 // ФУНКЦИИ
-// функция открытия попапа редактирования имени
+// функция открытия попапа
 const openPopup = (popupName) => {
   popupName.classList.add("popup_opened");
 };
@@ -39,10 +46,33 @@ function handleOpenPopupEdit() {
   openPopup(popupEdit);
 }
 
-// функция закрытия попапа редактирования имени
+// функция закрытия попапа
+
 const closePopup = (popupName) => {
   popupName.classList.remove("popup_opened");
 };
+
+
+//функция закрытия попапов по нажатию Esc
+document.addEventListener('keydown', function(event){
+ if (event.key === 'Escape'){
+    closePopup(popupEdit);
+    closePopup(popupCard);
+    closePopup(popupCardPhoto);
+  }
+});
+
+
+//функция закрытия попапа оверлеем
+function closePopupOverlayClick(evt){
+  if (evt.target === evt.currentTarget){
+    closePopup(popupEdit);
+    closePopup(popupCard);
+    closePopup(popupCardPhoto);
+  }
+}
+
+
 
 // функция отправки и кнопки "сохранить" в попапе редактирования имени
 function handleProfileFormSubmit(evt) {
@@ -52,6 +82,7 @@ function handleProfileFormSubmit(evt) {
   profileName.textContent = nameInput.value;
   closePopup(popupEdit);
 }
+
 
 // функция открытия попапа добавления карточек
 openPopupAddCard.addEventListener("click", () => openPopup(popupCard));
@@ -90,6 +121,31 @@ const handleAddCardFormSubmit = (event) => {
 };
 
 
+//функция показа ошибки
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`#error-${inputElement.id}`);
+  inputElement.classList.add('form__input_type_error');
+  errorElement.textContent = errorMessage;
+};
+
+// функция скрытия ошибки
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`#error-${inputElement.id}`);
+  inputElement.classList.remove('form__input_type_error');
+  errorElement.textContent = '';
+};
+
+
+// функция проверки валидности
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+
 // ОБРАБОТЧИКИ СОБЫТИЙ
 
 // кнопки открыть и закрыть для попапа редактирования
@@ -122,9 +178,6 @@ const generateCard = (initialData) => {
     openPopup(popupCardPhoto);
   };
 
-  
-
-
   elementPhoto.addEventListener("click",hendleTapCard);
 
   // like card
@@ -148,3 +201,69 @@ initialCards.forEach((initialData) => {
 });
 
 popupFormAdd.addEventListener("submit", handleAddCardFormSubmit);
+
+
+
+
+
+// 
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.form__input'));
+  const buttonElement = formElement.querySelector('.form__submit-btn');
+
+  toggleButtonState(inputList, buttonElement);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement);
+      
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+
+// функция переключения состояния кнопки
+const toggleButtonState = (inputList, buttonElement) =>{
+  if (hasInvalidInput(inputList)){
+    buttonElement.classList.add('form__button_inactive');
+  }else{
+    buttonElement.classList.remove('form__button_inactive');
+  };
+};
+
+
+// 
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.form'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+    });
+    
+    const fieldsetList = Array.from(formElement.querySelectorAll('.form__set'));
+   
+    fieldsetList.forEach((fieldSet) =>{
+     setEventListeners(fieldSet);  
+    });
+
+  });
+};
+
+
+//
+const hasInvalidInput = (inputList) =>{
+  return inputList.some((inputElement) => {
+  
+    return !inputElement.validity.valid;
+  })
+}; 
+
+enableValidation();
+
+
+// Слушатели закрытия оверлеем
+
+popupEdit.addEventListener('mousedown', closePopupOverlayClick); 
+popupCard.addEventListener('mousedown', closePopupOverlayClick); 
+popupCardPhoto.addEventListener('mousedown', closePopupOverlayClick); 
